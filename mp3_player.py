e99f8652e884184a164ff22f1ab37dab8acf87bb
sdcodeaesthetic <swarnadippramanik09@gmail.com>
2020-05-23 13:00:26 +0530

    change in file locations

--- mp3_player.py
+++ mp3_player.py
@@ -4,22 +4,23 @@ import pygame
 
 class MP3MusicPlayer():
     def __init__(self, master):
-        self.master = master
-        self.master.geometry("1000x200+200+200")
         pygame.init()
         pygame.mixer.init()
+        self.master = master
+        self.master.geometry("1000x200+200+200")
         self.track = StringVar()
-        self.status = 'None'
+        self.playlist = ''
+        self.songs_list = []
         self.gui_interfaces()
 
     def gui_interfaces(self):
         #Label Frame
-        upperframe = LabelFrame(self.master, text = "Song Track", font = ("calibri", 15, "bold"), bg = "grey", fg = "white", bd = 5, relief = GROOVE)
+        upperframe = LabelFrame(self.master, text = "Song Track", font = ("calibri", 15, "bold"), bg = "ivory4", fg = "white", bd = 5, relief = GROOVE)
         upperframe.place(x = 0, y = 0, width = 600, height = 100)
-        songtrack = Label(upperframe, textvariable = self.track, width = 30, font = ("calibri", 20, "bold"), bg = "grey", fg = "gold")
+        songtrack = Label(upperframe, textvariable = self.track, width = 40, font = ("calibri", 20, "bold"), bg = "ivory4", fg = "gold")
         songtrack.grid(row = 0, padx = 10, pady = 5)
         #Button Frame
-        bottomframe = LabelFrame(self.master, text = "Control Panel", font = ("calibri", 15, "bold"), bg = "grey", fg = "white", bd = 5, relief = GROOVE)
+        bottomframe = LabelFrame(self.master, text = "Control Panel", font = ("calibri", 15, "bold"), bg = "ivory4", fg = "white", bd = 5, relief = GROOVE)
         bottomframe.place(x = 0, y = 100, width = 600, height=100)
         playbtn = Button(bottomframe, text = "PLAY", width = 6, height = 1, font = ("calibri", 16, "bold"), fg = "blue", bg = "gold", command = lambda: self.play_song())
         playbtn.grid(row = 0, column = 0, padx = 10, pady = 5)
@@ -32,7 +33,7 @@ class MP3MusicPlayer():
         shufflebtn = Button(bottomframe, text = "SHUFFLE", width = 10, height = 1, font = ("calibri", 16, "bold"), fg = "blue", bg = "gold", command = lambda: self.shuffle_song())
         shufflebtn.grid(row = 0, column = 4, padx = 10, pady = 5)
         #Playlist Frame
-        songsframe = LabelFrame(self.master, text = "Song Playlist", font = ("calibri", 15, "bold"), bg = "grey", fg = "white", bd = 5, relief = GROOVE)
+        songsframe = LabelFrame(self.master, text = "Song Playlist", font = ("calibri", 15, "bold"), bg = "ivory4", fg = "white", bd = 5, relief = GROOVE)
         songsframe.place(x = 600, y = 0, width = 400, height = 200)
         scrol_y = Scrollbar(songsframe, orient = VERTICAL)
         self.playlist = Listbox(songsframe, yscrollcommand = scrol_y.set, selectbackground = "gold", selectmode = SINGLE, font = ("calibri", 12, "bold"), bg = "white smoke", fg = "blue", bd = 5, relief = GROOVE)
@@ -44,25 +45,47 @@ class MP3MusicPlayer():
         for directory in track_directory:
             os.chdir("F:\Music\{}".format(directory))
             songtracks = os.listdir()
-            for track in songtracks:     
-                self.playlist.insert(END, track)
+            self.songs_list.extend(songtracks)
+        self.songs_list.sort()
+        for song in self.songs_list:     
+            self.playlist.insert(END, song)
+
+    def finding_dir_path(self):
+        for root, dirs, files in os.walk("F:\Music"):
+            if self.playlist.get(ACTIVE) in files:
+                current_file = os.path.join(root, self.playlist.get(ACTIVE))
+        current_dir = os.path.split(os.path.abspath(current_file))
+        os.chdir(current_dir[0])
+        return current_dir[1]
+
+    '''def looping_song(self):
+        current_index = self.playlist.curselection()
+        self.playlist.activate(current_index[0] + 1)
+        self.track.set(self.playlist.get(ACTIVE))
+        pygame.mixer.music.load(self.finding_dir_path())
+        pygame.mixer.music.play()'''
 
     def play_song(self):
-        self.status = 'Playing'
-        self.track.set(self.playlist.get(ACTIVE))
-        pygame.mixer.music.load(self.playlist.get(ACTIVE))
+        #MUSIC_END = pygame.USEREVENT + 1
+        #pygame.mixer.music.set_endevent(MUSIC_END)
+        load_song_file = self.finding_dir_path()
+        self.track.set(load_song_file)
+        pygame.mixer.music.load(load_song_file)
         pygame.mixer.music.play()
+        '''while self.status == 'Playing':
+            for event in pygame.event.get():
+                if event.type == MUSIC_END:
+                    self.status = 'Stopped'
+                    break
+        self.looping_song()'''
 
     def pause_song(self):
-        self.status = 'Paused'
         pygame.mixer.music.pause()
 
     def unpause_song(self):
-        self.status = 'Playing'
         pygame.mixer.music.unpause()
 
     def stop_song(self):
-        self.status = 'Stopped'
         pygame.mixer.music.stop()
 
     def shuffle_song(self):
